20d0afda20b595ab5508fb4ce50d2575c6867cf4
Yutaro Mori <yutaro@umaproject.org>
2020-07-03 19:15:16 +0900

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import BurnerCore from "@burner-wallet/core";
 import LocalSigner from "@burner-wallet/core/signers/LocalSigner";
 import InjectedSigner from "@burner-wallet/core/signers/InjectedSigner";
 import InfuraGateway from "@burner-wallet/core/gateways/InfuraGateway";
-import HTTPGateway from "@burner-wallet/core/gateways/HTTPGateway";
 import { dai, eth } from "@burner-wallet/assets";
 
 import styled from "styled-components/macro";
@@ -17,18 +16,14 @@ const StyledApp = styled.div`
   display: flex;
 `;
 
-function App(props: any) {
+function App() {
   const [balance, setBalance] = useState<undefined | number>(undefined);
   const [core, setCore] = useState<any>(undefined);
 
   useEffect(() => {
-    const httpGateway =
-      process.env.REACT_APP_HTTP_GATEWAY || "http://localhost:8525";
-
     const core = new BurnerCore({
       signers: [new LocalSigner(), new InjectedSigner()],
       gateways: [new InfuraGateway(process.env.REACT_APP_INFURA_KEY)],
-      // gateways: [new HTTPGateway(httpGateway, "1")],
       assets: [dai, eth],
     });
 
